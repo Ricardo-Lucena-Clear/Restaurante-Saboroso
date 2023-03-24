1c5e9bd626c844e2e4fed71fc9767a878c721c31
Ricardo Lucena <ricardo.lucena.clear@gmail.com>
2023-03-24 10:58:11 -0300

    Organizing Content with Includes

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,18 @@
 var conn = require('./../inc/db')
 var express = require('express');
+
 var router = express.Router();
+var menus = require ('./../inc/menus')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  conn.query(`
-  SELECT * FROM tb_menus ORDER BY title`, (err, results)=>{
-    if (err){
-      console.log(err);
-    }
-    res.render('index', { title: 'Restaurante Saboroso', menus: results });
-  })
+  menus.getMenus().then(results=>{
 
-});
+    res.render('index', { title: 'Restaurante Saboroso', menus: results });
+
+  })
+    
+  })
 
 router.get('/contacts', function(req, res, next){
   res.render('contacts',{
@@ -23,10 +23,14 @@ router.get('/contacts', function(req, res, next){
 });
 
 router.get('/menus', function(req, res, next){
-  res.render('menus',{
+  menus.getMenus().then(results=>{
+     res.render('menus',{
     title: 'Menus - Restaurante Saboroso',
     background: 'images/img_bg_1.jpg',
     h1: 'Saboreie nosso menu!',
+    menus: results
+  })
+ 
   });
 });
 router.get('/reservations', function(req, res, next){
