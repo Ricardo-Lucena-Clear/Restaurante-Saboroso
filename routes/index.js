55871779d8350280b0b559d0a4356fdaff6bc24f
Ricardo Lucena <ricardo.lucena.clear@gmail.com>
2023-03-27 11:41:20 -0300

    Fix Reservation with POST Method for MySQL

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ var menus = require ('./../inc/menus')
 router.get('/', function(req, res, next) {
   menus.getMenus().then(results=>{
 
-    res.render('index', { title: 'Restaurante Saboroso', menus: results, isHome: true });
+    res.render('index', { title: 'Restaurante Saboroso', menus: results });
 
   })
     
@@ -43,18 +43,23 @@ router.post('/reservations', function(req, res, next){
   if (!req.body.name){
     reservations.render(req, res, "Digite o nome"); 
   }else if (!req.body.email){
-    res.send("Digite seu e-mail")
+    reservations.render(req, res, "Digite seu e-mail"); 
   }
   else if (!req.body.people){
-    res.send("Selecione o numero de pessoas")
+    reservations.render(req, res, "Selecione o numero de pessoas"); 
   }
   else if (!req.body.date){
-    res.send("Selecione a data")
+    reservations.render(req, res, "Selecione a data"); 
   }
   else if (!req.body.time){
-    res.send("Selecione o horário")
+    reservations.render(req, res, "Selecione o horário"); 
   }else{
-    res.send(req.body)
+    reservations.save(req.body).then(results =>{
+      req.body = {};
+      reservations.render(req, res, null, "Reserva realizada com sucesso!")
+    }).catch(err=>{
+      reservations.render(req, res, err.message);
+    })
   }
   
   });
