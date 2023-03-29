f78138793126b21dd7229977d8b162dc0e99b25a
Ricardo Lucena <ricardo.lucena.clear@gmail.com>
2023-03-29 09:06:58 -0300

    Menu Administration - Changing data via POST

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ app.use(function(req, res, next){
       keepExtensions: true
     });
     form.parse(req, function(err, fields, files){
+      req.body = fields;
       req.fields = fields;
       req.files = files;
       next();
@@ -44,7 +45,6 @@ app.use(session({
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
